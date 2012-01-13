18ffe2e24bcdac07bba9981f47e7669b742c243f
gareth <gareth@PaloAlto.(none)>
2012-01-12 19:28:03 -0500

    This allows the image page to be displayed in the facebook frame. We may use this.

--- web/image.py
+++ web/image.py
@@ -22,3 +22,19 @@ class ImageHandler(webapp.RequestHandler):
 
     self.response.out.write(template % params)
 
+  def post(self):
+    # receive image bytes
+    image_id = self.request.get('id', '' )
+    meme_name = self.request.get('meme_name', '')
+
+    imgur_link = 'http://i.imgur.com/' + image_id
+
+    params = {
+      'title': meme_name,
+      'image_url': imgur_link,
+      'page_url': self.request.url,
+    }
+
+    template = open('static/html/image.html').read()
+
+    self.response.out.write(template % params)
